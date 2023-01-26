291552ebcac8622a5f4471fd498510d0f72b709d
Shashank Lokesha Reddy <shashank.lokesha@iem.fraunhofer.de>
2023-01-26 14:21:38 +0000

    Update frontend/src/views/OrderTable.vue

--- frontend/src/views/OrderTable.vue
+++ frontend/src/views/OrderTable.vue
@@ -36,18 +36,19 @@ export default {
   beforeMount() {
       this.columnDefs = [
         { headerName: "Maschine", field: "resourceId", type: 'rightAligned', filter: true },
-        { headerName: "Kundennummer", field: "title", type: 'rightAligned', filter:true},
+        { headerName: "Job", field: "jobID", type: 'rightAligned', filter:true},
+        { headerName: "Part ID", field: "partID", type: 'rightAligned', filter:true },
         { headerName: "Startzeit", field: "start", type: 'rightAligned', filter:true },
         { headerName: "Endzeit", field: "end", type: 'rightAligned', filter:true },
-        { headerName: "Auftragsnummer", field: "AKNR", type: 'rightAligned', filter:true },
-        { headerName: "Schrittnummer", field: "SchrittNr", type: 'rightAligned', filter:true },
+        { headerName: "Production Start", field: "productionStart", type: 'rightAligned', filter:true },
+        { headerName: "Production End", field: "productionEnd", type: 'rightAligned', filter:true },
       ];
      /* this.rowData =[
         { make: "Toyota", model: "Celica", price: 35000 },
         { make: "Ford", model: "Mondeo", price: 32000 },
         { make: "Porsche", model: "Boxster", price: 72000 },
       ];*/
-      fetch('http://localhost:8000/api/machines/')
+      fetch('http://localhost:8000/api/jobs/')
         .then(res => res.json())
         .then(rowData => this.rowData = rowData)
         .catch(error => console.log(error));
